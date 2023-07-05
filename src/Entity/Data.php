20efd1657ac27ea13a078682247c7cc03cc6c4ac
Andry <blader_100@mail.ru>
2023-07-05 19:27:39 +0300

    Added api Data

--- src/Entity/Data.php
+++ src/Entity/Data.php
@@ -2,32 +2,51 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use App\Repository\DataRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: DataRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext: ['groups' => 'data:item']),
+        new GetCollection(normalizationContext: ['groups' => 'data:list'])
+    ],
+    paginationEnabled: false,
+)]
 class Data
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['data:list', 'data:item'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['data:list', 'data:item'])]
     private ?string $Name = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['data:list', 'data:item'])]
     private ?string $Data = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    #[Groups(['data:list', 'data:item'])]
     private ?\DateTimeInterface $RecordingDate = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['data:list', 'data:item'])]
     private ?string $file = null;
 
     #[ORM\ManyToOne(inversedBy: 'data',targetEntity: User::class)]
     #[ORM\JoinColumn(nullable:true,  onDelete: "CASCADE")]
+    #[Groups(['data:list', 'data:item'])]
     private ?User $user = null;
 
 
